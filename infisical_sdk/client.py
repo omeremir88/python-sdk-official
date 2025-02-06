1f47da7dd301239cf94da77fa59750a3fe408e29
Scott Wilson <scottraywilson@gmail.com>
2025-02-05 17:41:17 -0800

    feature: KMS operation support + docs

--- infisical_sdk/client.py
+++ infisical_sdk/client.py
@@ -12,7 +12,8 @@ from botocore.awsrequest import AWSRequest
 from botocore.exceptions import NoCredentialsError
 
 from .infisical_requests import InfisicalRequests
-from .api_types import ListSecretsResponse, MachineIdentityLoginResponse
+from .api_types import ListSecretsResponse, MachineIdentityLoginResponse, ListKmsKeysResponse, SingleKmsKeyResponse, \
+    KmsKey, SymmetricEncryption, KmsKeyEncryptDataResponse, KmsKeyDecryptDataResponse, KmsKeysOrderBy, OrderDirection
 from .api_types import SingleSecretResponse, BaseSecret
 
 
@@ -25,6 +26,7 @@ class InfisicalSDKClient:
 
         self.auth = Auth(self)
         self.secrets = V3RawSecrets(self)
+        self.kms = KMS(self)
 
     def set_token(self, token: str):
         """
@@ -307,7 +309,7 @@ class V3RawSecrets:
           "secretPath": secret_path,
           "secretValue": secret_value,
           "secretComment": secret_comment,
-          "new_secret_name": new_secret_name,
+          "newSecretName": new_secret_name,
           "tagIds": None,
           "skipMultilineEncoding": skip_multiline_encoding,
           "type": "shared",
@@ -343,3 +345,175 @@ class V3RawSecrets:
         )
 
         return result.data.secret
+
+class KMS:
+    def __init__(self, client: InfisicalSDKClient) -> None:
+        self.client = client
+
+    def list_keys(
+            self,
+            project_id: str,
+            offset: int = 0,
+            limit: int = 100,
+            order_by: KmsKeysOrderBy = KmsKeysOrderBy.NAME,
+            order_direction: OrderDirection = OrderDirection.ASC,
+            search: str = None) -> ListKmsKeysResponse:
+
+        params = {
+            "projectId": project_id,
+            "search": search,
+            "offset": offset,
+            "limit": limit,
+            "orderBy": order_by,
+            "orderDirection": order_direction,
+        }
+
+        result = self.client.api.get(
+            path="/api/v1/kms/keys",
+            params=params,
+            model=ListKmsKeysResponse
+        )
+
+        return result.data
+
+    def get_key_by_id(
+            self,
+            key_id: str) -> KmsKey:
+
+        result = self.client.api.get(
+            path=f"/api/v1/kms/keys/{key_id}",
+            model=SingleKmsKeyResponse
+        )
+
+        return result.data.key
+
+    def get_key_by_name(
+            self,
+            key_name: str,
+            project_id: str) -> KmsKey:
+
+        params = {
+            "projectId": project_id,
+        }
+
+        result = self.client.api.get(
+            path=f"/api/v1/kms/keys/key-name/{key_name}",
+            params=params,
+            model=SingleKmsKeyResponse
+        )
+
+        return result.data.key
+
+    def create_key(
+            self,
+            name: str,
+            project_id: str,
+            encryption_algorithm: SymmetricEncryption,
+            description: str = None) -> KmsKey:
+
+        request_body = {
+            "name": name,
+            "projectId": project_id,
+            "encryptionAlgorithm": encryption_algorithm,
+            "description": description,
+        }
+
+        result = self.client.api.post(
+            path="/api/v1/kms/keys",
+            json=request_body,
+            model=SingleKmsKeyResponse
+        )
+
+        return result.data.key
+
+    def update_key(
+            self,
+            key_id: str,
+            name: str = None,
+            is_disabled: bool = None,
+            description: str = None) -> KmsKey:
+
+        request_body = {
+            "name": name,
+            "isDisabled": is_disabled,
+            "description": description,
+        }
+
+        result = self.client.api.patch(
+            path=f"/api/v1/kms/keys/{key_id}",
+            json=request_body,
+            model=SingleKmsKeyResponse
+        )
+
+        return result.data.key
+
+    def delete_key(
+            self,
+            key_id: str) -> KmsKey:
+
+        result = self.client.api.delete(
+            path=f"/api/v1/kms/keys/{key_id}",
+            json={},
+            model=SingleKmsKeyResponse
+        )
+
+        return result.data.key
+
+    def encrypt_data(
+            self,
+            key_id: str,
+            plaintext: str) -> str:
+        """
+            Encrypt data with the specified KMS key.
+
+            :param key_id: The ID of the key to decrypt the ciphertext with
+            :type key_id: str
+            :param plaintext: The base64 encoded plaintext to encrypt
+            :type plaintext: str
+
+
+            :return: The encrypted base64 encoded plaintext (ciphertext)
+            :rtype: str
+        """
+
+        request_body = {
+            "plaintext": plaintext
+        }
+
+        result = self.client.api.post(
+            path=f"/api/v1/kms/keys/{key_id}/encrypt",
+            json=request_body,
+            model=KmsKeyEncryptDataResponse
+        )
+
+        return result.data.ciphertext
+
+    def decrypt_data(
+            self,
+            key_id: str,
+            ciphertext: str) -> str:
+        """
+            Decrypt data with the specified KMS key.
+
+            :param key_id: The ID of the key to decrypt the ciphertext with
+            :type key_id: str
+            :param ciphertext: The encrypted base64 plaintext to decrypt
+            :type ciphertext: str
+
+
+            :return: The base64 encoded plaintext
+            :rtype: str
+        """
+
+        request_body = {
+            "ciphertext": ciphertext
+        }
+
+
+        result = self.client.api.post(
+            path=f"/api/v1/kms/keys/{key_id}/decrypt",
+            json=request_body,
+            model=KmsKeyDecryptDataResponse
+        )
+
+        return result.data.plaintext
